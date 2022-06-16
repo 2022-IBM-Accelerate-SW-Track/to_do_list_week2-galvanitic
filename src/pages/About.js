ff6ca36629e0969109bde5be388732bc38a913e1
galvanitic <develop@rglvn.com>
2022-06-15 22:01:46 -0500

    Completed project.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,31 +1,42 @@
-import React, { Component } from "react";
-import "./About.css";
-import profile_pic from "../assets/profile_pic.jpg";
-
+import React, { Component } from 'react';
+import './About.css';
+import ProfilePicture from '../assets/profile.jpg';
+  
 export default class About extends Component {
   render() {
     return (
       <div>
-        {/* <p>Design your About me page </p> */}
         <div class="split left">
           <div className="centered">
-            <img
+            <img 
               className="profile_image"
-              src={profile_pic}
+              src={ProfilePicture}
               alt="Profile Pic"
-            ></img>
+              >
+            </img>
           </div>
         </div>
-        <div className="split right">
-          <div className="centered">
-            <div className="name_title">Your Name</div>
-            <div className="brief_description">
-              Tell us about yourself in a few sentences. Tell us your interests
-              and say a fun fact about yourself.
-            </div>
-          </div>
+      <div className="split right">
+      <div className="centered">
+        <div className="name_title">Rodolfo J. Galván Martínez</div>
+        <div className="brief_description">
+          <p>
+            Born in León Guanajuato, México and raised in Austin, TX, Rodolfo is a post-traditional computer science and computer engineering student at Texas State University. 
+            He is passionate about equitable technology and empowering others in his community. 
+          </p>
+          <p>
+            Rodolfo serves on the board of directors of Explore Austin, a nonprofit that empowers historically underserved youth to reach their full potential through outdoor adventure.
+            He helps out PelotonU, a nonprofit that provides post-traditional students the pathway and support through college. In 2018 he also co-founded and lead a 501(c)3 organization, Código Austin:
+            that promotes STEM fields to students from historically underserved communities.
+          </p>
+          <p>
+            Rodolfo specializes in equitable and accessible cloud-based full-stack software development. Apart from his leadership and entrepreneurial experience, he also has technical knowledge in hardware-level computer architecture/design and computer science principles.
+          </p>
+
         </div>
       </div>
-    );
+    </div>
+      </div>
+    )
   }
 }
